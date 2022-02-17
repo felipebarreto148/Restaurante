fedace39afbcd4a90ffa54aefdc2ec9ad43b941d
Felipe Barreto <felipebarreto148@gmail.com>
2022-02-17 19:12:29 -0300

    feat: Main pages created

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="home">
+    <Header title="Cardápio" />
+    <Content />
+  </main>
+</template>
+
+<script>
+// Styles
+import "../scss/global.scss";
+import "../scss/components/home.scss";
+
+export default {
+  name: 'IndexPage',
+
+}
+</script>
