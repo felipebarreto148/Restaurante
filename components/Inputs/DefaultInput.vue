bafb260c8588ad15d5fda6c17cb37686dca9c524
Felipe Barreto <felipebarreto148@gmail.com>
2022-02-18 12:27:54 -0300

    feat: Default input Created

---
+++ components/Inputs/DefaultInput.vue
@@ -0,0 +1,41 @@
+<template>
+	<section class="defaultInput">
+		<label :for="id">{{ text }} </label>
+		<input :type="type" :name="name" :id="id" :placeholder="placeholder"/>
+	</section>
+</template>
+
+<script>
+// Style
+import '../../scss/components/Inputs/defaultInput.scss'
+
+export default {
+	name: 'DefaultInput',
+	props: {
+		type: {
+			type: String,
+			default: () => 'text',
+		},
+		text: {
+			type: String,
+			required: true,
+			default: () => 'Título',
+		},
+		name: {
+			type: String,
+			required: true,
+			default: () => 'defaultInput',
+		},
+		id: {
+			type: String,
+			required: true,
+			default: () => 'defaultInput',
+		},
+		placeholder: {
+			type: String,
+			required: true,
+			default: () => 'Título do produto',
+		},
+	},
+}
+</script>
