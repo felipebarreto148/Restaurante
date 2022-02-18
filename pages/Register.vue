20754deddf0c9224b65542b53e06dca7179177f6
Felipe Barreto <felipebarreto148@gmail.com>
2022-02-18 17:10:49 -0300

    feat: Methods to create a product created

--- pages/Register.vue
+++ pages/Register.vue
@@ -5,18 +5,22 @@
 			<section class="registerProduct full">
 				<section class="fields">
 					<DefaultInput
+						@inputed="(value) => (product.title = value)"
 						name="inpTitle"
 						id="inpTitle"
 						text="Título"
 						placeholder="titulo do produto"
 					/>
 					<DefaultInput
+						@inputed="(value) => (product.description = value)"
 						name="inpDescription"
 						id="inpDescription"
 						text="Descrição"
 						placeholder="Descrição do produto"
 					/>
 					<DefaultInput
+						@inputed="(value) => (product.price = value)"
+						type="number"
 						name="inpPrice"
 						id="inpPrice"
 						text="Valor"
@@ -25,7 +29,7 @@
 				</section>
 				<section class="buttons w-full">
 					<DefaultButton variant isLink link="/" text="Cancelar" />
-					<DefaultButton main isLink link="/register" text="Salvar" />
+					<DefaultButton main text="Salvar" @clicked="() => register()" />
 				</section>
 			</section>
 		</Content>
@@ -37,12 +41,30 @@
 import '../scss/global.scss'
 import '../scss/components/home.scss'
 import '../scss/components/registerPage.scss'
-
+// Store
+import { mapActions } from 'vuex'
 export default {
 	name: 'Register',
 	components: {
 		DefaultInput: () => import('../components/Inputs/DefaultInput.vue'),
 		DefaultButton: () => import('../components/Buttons/DefaultButton.vue'),
+	},
+	data() {
+		return {
+			product: {
+				title: '',
+				description: '',
+				price: 0,
+			},
+		}
+	},
+	methods: {
+		...mapActions(['newProduct']),
+		async register() {
+			await this.newProduct(this.product).then(() => {
+				this.$router.push('/')
+			})
+		},
 	},
 }
 </script>
